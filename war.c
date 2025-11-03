954417462544f964dc1aefa166716b92507baec3
Robsontmb <rtadeumb@hotmail.com>
2025-11-03 23:00:00 +0000

    sim

--- war.c
+++ war.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_TERRITORIOS = 5
+
 struct territorio {
 
-char nome [15];
+char nome [30];
 char cor [10];
 int tropas;
 
@@ -12,7 +14,25 @@ int tropas;
 
 };
 
+void limparbufferentrada() {
+    int c;
+    while ((c=getchar()) != '\n' && c != EOF);
+}
+
+
 int main() {
+    struct territorio continentes[MAX_TERRITORIOS];
+    int totalterritorios = 0;
+    int opção = 0;
+
+    do {
+
+
+
+
+        
+    }
+
 
 printf("Insira o nome do território: \n");
 
